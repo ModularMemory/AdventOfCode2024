b780549afe77b39476bcb9f3068559417ca42cc6
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-12-04 18:21:31 -0500

    Day 4 part 2 completed

--- Day 4/main.cpp
+++ Day 4/main.cpp
@@ -87,6 +87,35 @@ static bool is_down_right(vector<string> lines, size_t x, size_t y) {
         && lines[y + 3][x + 3] == 'S';
 }
 
+static bool is_x_mas(vector<string> lines, size_t x, size_t y) {
+    if (y == 0 || y + 1 >= lines.size() || x == 0 || x + 1 >= lines[y].size()) {
+        return false;
+    }
+
+    if (lines[y][x] != 'A') {
+        return false;
+    }
+
+    if (lines[y - 1][x - 1] == 'M') {
+        if (lines[y + 1][x - 1] == 'M') {
+            return lines[y + 1][x + 1] == 'S' && lines[y - 1][x + 1] == 'S';
+        }
+        else if (lines[y + 1][x - 1] == 'S') {
+            return lines[y + 1][x + 1] == 'S' && lines[y - 1][x + 1] == 'M';
+        }
+    }
+    else if (lines[y - 1][x - 1] == 'S') {
+        if (lines[y + 1][x - 1] == 'M') {
+            return lines[y + 1][x + 1] == 'M' && lines[y - 1][x + 1] == 'S';
+        }
+        else if (lines[y + 1][x - 1] == 'S') {
+            return lines[y + 1][x + 1] == 'M' && lines[y - 1][x + 1] == 'M';
+        }
+    }
+    
+    return false;
+}
+
 int main(void) {
     auto ifs = ifstream(INPUT_FILENAME);
     auto input = string(istreambuf_iterator<char>(ifs), istreambuf_iterator<char>());
@@ -96,43 +125,11 @@ int main(void) {
     for (auto y = 0; y < lines.size(); y++) {
         auto& line = lines[y];
         for (auto x = 0; x < line.size(); x++) {
-            if (line[x] != 'X') {
+            if (line[x] != 'A') {
                 continue;
             }
 
-            if (line.substr(x).find("XMAS", 0) == 0) {
-                xmas++;
-            }
-
-            if (x >= 3
-                && line[x] == 'X'
-                && line[x - 1] == 'M'
-                && line[x - 2] == 'A'
-                && line[x - 3] == 'S') {
-                xmas++;
-            }
-
-            if (is_up(lines, x, y)) {
-                xmas++;
-            }
-
-            if (is_down(lines, x, y)) {
-                xmas++;
-            }
-
-            if (is_up_left(lines, x, y)) {
-                xmas++;
-            }
-
-            if (is_up_right(lines, x, y)) {
-                xmas++;
-            }
-
-            if (is_down_left(lines, x, y)) {
-                xmas++;
-            }
-
-            if (is_down_right(lines, x, y)) {
+            if (is_x_mas(lines, x, y)) {
                 xmas++;
             }
         }
