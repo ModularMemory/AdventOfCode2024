eab4d09a63f15dcf180d917cba560f86430fe7ff
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-12-11 22:54:52 -0500

    Day 9 part 2 completed

--- Day 9/main.cpp
+++ Day 9/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -71,12 +72,89 @@ static u64 part1(const vector<i32> disk) {
     return checksum;
 }
 
+static u64 compute_checksum(vector<i32> disk) {
+    auto checksum = 0ULL;
+    for (auto pos = 0ULL; pos < disk.size(); pos++) {
+        auto id = disk[pos];
+        if (id == -1) {
+            continue;
+        }
+
+        checksum += pos * id;
+    }
+
+    return checksum;
+}
+
+static void defragment_files(vector<i32>& disk) {
+    auto firstAvailable = 0ULL;
+    for (auto end = disk.size() - 1; end >= firstAvailable; end--) {
+        auto id = disk[end];
+        if (id == -1) {
+            continue;
+        }
+
+        // Check file size
+        auto size = 1;
+        while (end - size >= firstAvailable && disk[end - size] == id) {
+            size++;
+        }
+
+        for (auto start = firstAvailable;; start++) {
+            if (start >= end) {
+                end -= size - 1;
+                break;
+            }
+
+            if (disk[start] != -1) {
+                continue;
+            }
+
+            // Check available space
+            auto available = 1;
+            while (available < size && start + available <= end && disk[start + available] == -1) {
+                available++;
+            }
+
+            if (available < size) {
+                continue;
+            }
+
+            // Move file
+            for (auto i = 0; i < size; i++) {
+                disk[start + i] = disk[end - i];
+                disk[end - i] = -1;
+            }
+            
+            // Find next available
+            auto it = find(disk.begin(), disk.end(), -1);
+            if (it == disk.end()) {
+                cerr << "Failed to find free space." << endl;
+                return;
+            }
+
+            firstAvailable = distance(disk.begin(), it);
+            break;
+        }
+    }
+}
+
+static u64 part2(const vector<i32> original_disk) {
+    auto disk = vector<i32>(original_disk);
+
+    defragment_files(disk);
+
+    return compute_checksum(disk);
+}
+
 int main(void) {
     auto disk = get_disk();
 
-    auto sum1 = part1(disk);
+    //auto sum1 = part1(disk);
+    auto sum2 = part2(disk);
 
-    cout << sum1 << endl;
+    //cout << sum1 << endl;
+    cout << sum2 << endl;
 
     return 0;
 }
