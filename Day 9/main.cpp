8564281004cfbc09d776b04da62f5be2b8bc9f1f
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-12-11 20:21:17 -0500

    Day 9 part 1 completed

---
+++ Day 9/main.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+static int prase_int(char c) {
+    if (c >= '0' && c <= '9') {
+        return c - '0';
+    }
+
+    cerr << "Failed to parse '" << c << "' as int." << endl;
+    exit(1);
+}
+
+static vector<unsigned long long> get_disk() {
+    auto ifs = ifstream("inputs.txt");
+    auto input = string(istreambuf_iterator<char>(ifs), istreambuf_iterator<char>());
+
+    auto disk = vector<unsigned long long>();
+    auto id = 0ULL;
+    for (auto i = 0ULL; i < input.size(); i++) {
+        auto count = prase_int(input[i]);
+        if (i % 2 != 0) {
+            for (auto j = 0; j < count; j++) {
+                disk.push_back(-1);
+            }
+        }
+        else {
+            for (auto j = 0; j < count; j++) {
+                disk.push_back(id);
+            }
+            id++;
+        }
+    }
+
+    return disk;
+}
+
+static unsigned long long part1(const vector<unsigned long long> disk) {
+    auto start = 0ULL;
+    auto end = disk.size() - 1;
+    auto checksum = 0ULL;
+    auto pos = 0ULL;
+    while (start <= end) {
+        auto i = disk[start];
+        if (i != -1) {
+            checksum += pos * i;
+        }
+        else {
+            while (disk[end] == -1) {
+                end--;
+            }
+
+            if (end < start) {
+                break;
+            }
+
+            checksum += pos * disk[end];
+            end--;
+        }
+
+        start++;
+        pos++;
+    }
+
+    return checksum;
+}
+
+int main(void) {
+    auto disk = get_disk();
+
+    auto sum1 = part1(disk);
+
+    cout << sum1 << endl;
+
+    return 0;
+}
